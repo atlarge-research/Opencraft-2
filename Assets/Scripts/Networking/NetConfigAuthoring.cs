5445f430649856fae4700cba9b1213a15810c8f6
jeickhoff <j.d.eickhoff@student.tudelft.nl>
2023-05-29 16:59:33 +0200

    Add NetConfig in subscene

---
+++ Assets/Scripts/Networking/NetConfigAuthoring.cs
@@ -0,0 +1,23 @@
+using Unity.Entities;
+using Unity.NetCode;
+using UnityEngine;
+
+
+
+public class NetConfigAuthoring  : MonoBehaviour
+{
+    public int NetworkTickRate;
+    public int SimulationTickRate;
+    class Baker : Baker<NetConfigAuthoring>
+    {
+        public override void Bake(NetConfigAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            var clientServerTickRate = new ClientServerTickRate(){NetworkTickRate =  authoring.NetworkTickRate,
+                SimulationTickRate = authoring.SimulationTickRate};
+            clientServerTickRate.ResolveDefaults();
+            AddComponent(entity, clientServerTickRate);
+        }
+    }
+}
+
