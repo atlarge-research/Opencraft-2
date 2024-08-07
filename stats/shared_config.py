a4f0578c5c4625eee149be5df4b4c12659501822
ZainMunir <zainmunir999@gmail.com>
2024-08-07 15:39:53 +0100

    cleanup

--- stats/shared_config.py
+++
@@ -1,7 +0,0 @@
-config = {
-    "terrainTypes": {"a":"1-Layer", "b":"2-Layer", "c":"3-Layer", "d":"Empty", "e":"RollingHills", "f":"TerrainCircuitry"},
-    "num_sims": [1],#, 5, 10],
-    "iterations": 1,
-    "tolerance": 10,
-    "sim_clients_duration": 30,
-}
